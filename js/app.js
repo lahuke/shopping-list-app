4ebf47fbc8f832ef24f0a2c2dd41466867b4684c
Luke Tucker <luke@luketucker.com>
2015-04-21 23:14:06 -1000

    JQuery best efforts

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
- $(document).ready(function () {
+$(document).ready(function () {
 
     /*Defining global variables*/
     console.log('Welcome to console');
     var submissions = 0;
-    var checkMark = '<span class="cross-off"></span>'
-    var xMark = '<span class="delete"></span>'
 
 
     /*Allows an "Enter" keystroke to work as a submit button for input field*/
-    function getItem() {
+    function getItem(){
         $('#add-items').keydown(function (enter) {
             if (enter.keyCode == 13) {
                 postItem();
@@ -16,38 +14,14 @@
         });
     }
 
-    getItem();
-
     /*Takes returned value from previous function and manipulates it*/
-    function postItem() {
+    function postItem(){
         var item = $('#add-items').val();
-        var work = '<p class="full-width no-strikethrough">'+ xMark + item + checkMark + '</p>';
-        $('#list-area').prepend(work);
+        var work = $('<p class="full-width no-strikethrough">'+ xMark + item + checkMark + '</p>';);
+        $('.rounded-list').prepend(work);
         $('#add-items').val('');
-        $('#list-area p:first-child')
-        .css('opacity', "0")
-        .css("margin-top", "-20px")
-        .animate(
-            { opacity: "1" },
-            { queue: true, duration: 'slow' }
-        )
-        .animate(
-            {marginTop: "0px"},
-            { queue: false, duration: 'slow' }
-        );
-    }
-
-    function crossOff(){
-        $('.cross-off').toggle(function () {
-            $(this).closest('p').addClass("strikethrough");
-            console.log("Stricken")
-        }, function () {
-            $(this).closest('p').removeClass("active");
-        });
-    }
-
-
-});
+        $('.rounded-list p:first-child');
+    });
 
 /*Clears all items on list*/
 $(document).on("click", "#startover", function(){
@@ -57,21 +31,22 @@ $(document).on("click", "#startover", function(){
 
 /*Allows user to delete items*/
 $(document).on("click", ".delete", function(){
-    $(this).closest('p').fadeOut(300);
+    $(this).closest('li').fadeOut(300);
 });
 
 /*Allows user to check off items*/
 $(document).on("click", ".cross-off", function(){
-    if (!$(this).closest('p').hasClass('strikethrough')) {
+    if (!$(this).closest('li').hasClass('strikethrough')) {
         console.log("I am going to add a strikethrough");
-        $(this).closest('p').addClass("strikethrough");
+        $(this).closest('li').addClass("strikethrough");
         $(this).siblings('.delete').addClass("alt-delete");
         $(this).addClass("alt-cross-off");
    }
    else {
         console.log("I am going to remove a strikethrough");
-        $(this).closest('p').removeClass("strikethrough");
+        $(this).closest('li').removeClass("strikethrough");
         $(this).siblings('.delete').removeClass("alt-delete");
         $(this).removeClass("alt-cross-off");
    }
+   
 });
